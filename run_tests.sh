d3dcabd23e73e8be3c7538569ac236e87d5fcea3
Rasmus Bondesson <raek@raek.se>
2019-12-27 23:54:57 +0100

    Print name of test before running it

--- run_tests.sh
+++ run_tests.sh
@@ -11,6 +11,7 @@ function error() {
 }
 
 function check() {
+    echo "$1"
     (cd test && avr-objdump -b binary -m avr -D "$1".bin > "$1"_dis.txt) || error "disassembly failed"
     python3 rasm.py test/"$1".s -o test_out/"$1".bin || error "rasm.py failed"
     (cd test_out && avr-objdump -b binary -m avr -D "$1".bin > "$1"_dis.txt) || error "disassembly failed"
