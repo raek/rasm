8bb89b67611c0364574f4a76026934d8b283f569
Rasmus Bondesson <raek@raek.se>
2020-01-05 19:50:51 +0100

    Refactor to allow multitple addressing modes

--- rasm.py
+++ rasm.py
@@ -56,116 +56,130 @@ OPERAND_TYPES = {
 }
 
 
-INSTRUCTIONS = {
-    "nop":    ("  ", "0000 0000 0000 0000"),
-    "movw":   ("pp", "0000 0001 aaaa bbbb"),
-    "muls":   ("hh", "0000 0010 aaaa bbbb"),
-    "mulsu":  ("HH", "0000 0011 0aaa 0bbb"),
-    "fmul":   ("HH", "0000 0011 0aaa 1bbb"),
-    "fmulsu": ("HH", "0000 0011 1aaa 1bbb"),
-    "fmuls":  ("HH", "0000 0011 1aaa 0bbb"),
-    "cpc":    ("rr", "0000 01ba aaaa bbbb"),
-    "sbc":    ("rr", "0000 10ba aaaa bbbb"),
-    "add":    ("rr", "0000 11ba aaaa bbbb"),
-    "lsl":    ("r ", "0000 11Aa aaaa AAAA"),
-    "cpse":   ("rr", "0001 00ba aaaa bbbb"),
-    "cp":     ("rr", "0001 0110 0000 1111"),
-    "sub":    ("rr", "0001 10ba aaaa bbbb"),
-    "adc":    ("rr", "0001 11ba aaaa bbbb"),
-    "rol":    ("r ", "0001 11Aa aaaa AAAA"),
-    "and":    ("rr", "0010 00ba aaaa bbbb"),
-    "tst":    ("r ", "0010 00Aa aaaa AAAA"),
-    "eor":    ("rr", "0010 01ba aaaa bbbb"),
-    "clr":    ("r ", "0010 01Aa aaaa AAAA"),
-    "or":     ("rr", "0010 10ba aaaa bbbb"),
-    "mov":    ("rr", "0010 11ba aaaa bbbb"),
-    "cpi":    ("hk", "0011 bbbb aaaa bbbb"),
-    "sbci":   ("hk", "0100 bbbb aaaa bbbb"),
-    "subi":   ("hk", "0101 bbbb aaaa bbbb"),
-    "ori":    ("hk", "0110 bbbb aaaa bbbb"),
-    "sbr":    ("hk", "0110 bbbb aaaa bbbb"),
-    "andi":   ("hk", "0111 bbbb aaaa bbbb"),
-    "cbr":    ("hK", "0111 bbbb aaaa bbbb"),
-    "lds":    ("rA", "1001 000a aaaa 0000 bbbb bbbb bbbb bbbb"),
-    "pop":    ("r ", "1001 000a aaaa 1111"),
-    "sts":    ("Ar", "1001 001b bbbb 0000 aaaa aaaa aaaa aaaa"),
-    "push":   ("r ", "1001 001a aaaa 1111"),
-    "ijmp":   ("  ", "1001 0100 0000 1001"),
-    "des":    ("k ", "1001 0100 aaaa 1011"),
-    "com":    ("r ", "1001 010a aaaa 0000"),
-    "neg":    ("r ", "1001 010a aaaa 0001"),
-    "swap":   ("r ", "1001 010a aaaa 0010"),
-    "inc":    ("r ", "1001 010a aaaa 0011"),
-    "asr":    ("r ", "1001 010a aaaa 0101"),
-    "lsr":    ("r ", "1001 010a aaaa 0110"),
-    "ror":    ("r ", "1001 010a aaaa 0111"),
-    "dec":    ("r ", "1001 010a aaaa 1010"),
-    "jmp":    ("A ", "1001 010a aaaa 110a aaaa aaaa aaaa aaaa"),
-    "call":   ("A ", "1001 010a aaaa 111a aaaa aaaa aaaa aaaa"),
-    "bset":   ("b ", "1001 0100 0aaa 1000"),
-    "sec":    ("  ", "1001 0100 0000 1000"),
-    "sez":    ("  ", "1001 0100 0001 1000"),
-    "eijmp":  ("  ", "1001 0100 0001 1001"),
-    "sen":    ("  ", "1001 0100 0010 1000"),
-    "sev":    ("  ", "1001 0100 0011 1000"),
-    "ses":    ("  ", "1001 0100 0100 1000"),
-    "seh":    ("  ", "1001 0100 0101 1000"),
-    "set":    ("  ", "1001 0100 0110 1000"),
-    "sei":    ("  ", "1001 0100 0111 1000"),
-    "bclr":   ("b ", "1001 0100 1aaa 1000"),
-    "clc":    ("  ", "1001 0100 1000 1000"),
-    "clz":    ("  ", "1001 0100 1001 1000"),
-    "cln":    ("  ", "1001 0100 1010 1000"),
-    "clv":    ("  ", "1001 0100 1011 1000"),
-    "cls":    ("  ", "1001 0100 1100 1000"),
-    "clh":    ("  ", "1001 0100 1101 1000"),
-    "clt":    ("  ", "1001 0100 1110 1000"),
-    "cli":    ("  ", "1001 0100 1111 1000"),
-    "ret":    ("  ", "1001 0101 0000 1000"),
-    "icall":  ("  ", "1001 0101 0000 1001"),
-    "reti":   ("  ", "1001 0101 0001 1000"),
-    "eicall": ("  ", "1001 0101 0001 1001"),
-    "sleep":  ("  ", "1001 0101 1000 1000"),
-    "break":  ("  ", "1001 0101 1001 1000"),
-    "wdr":    ("  ", "1001 0101 1010 1000"),
-    "adiw":   ("Pk", "1001 0110 bbaa bbbb"),
-    "sbiw":   ("Pk", "1001 0111 bbaa bbbb"),
-    "cbi":    ("ib", "1001 1000 aaaa abbb"),
-    "sbic":   ("ib", "1001 1001 aaaa abbb"),
-    "sbi":    ("ib", "1001 1010 aaaa abbb"),
-    "sbis":   ("ib", "1001 1011 aaaa abbb"),
-    "mul":    ("rr", "1001 11ba aaaa bbbb"),
-    "in":     ("rI", "1011 0bba aaaa bbbb"),
-    "out":    ("Ir", "1011 1aab bbbb aaaa"),
-    "rjmp":   ("a ", "1100 aaaa aaaa aaaa"),
-    "rcall":  ("a ", "1101 aaaa aaaa aaaa"),
-    "ldi":    ("rk", "1110 bbbb aaaa bbbb"),
-    "ser":    ("r ", "1110 1111 aaaa 1111"),
-    "brbs":   ("ba", "1111 00bb bbbb baaa"),
-    "brcs":   ("a ", "1111 00aa aaaa a000"),
-    "brlo":   ("a ", "1111 00aa aaaa a000"),
-    "breq":   ("a ", "1111 00aa aaaa a001"),
-    "brmi":   ("a ", "1111 00aa aaaa a010"),
-    "brvs":   ("a ", "1111 00aa aaaa a011"),
-    "brlt":   ("a ", "1111 00aa aaaa a100"),
-    "brhs":   ("a ", "1111 00aa aaaa a101"),
-    "brts":   ("a ", "1111 00aa aaaa a110"),
-    "brie":   ("a ", "1111 00aa aaaa a111"),
-    "brbc":   ("ba", "1111 01bb bbbb baaa"),
-    "brcc":   ("a ", "1111 01aa aaaa a000"),
-    "brsh":   ("a ", "1111 01aa aaaa a000"),
-    "brne":   ("a ", "1111 01aa aaaa a001"),
-    "brpl":   ("a ", "1111 01aa aaaa a010"),
-    "brvc":   ("a ", "1111 01aa aaaa a011"),
-    "brge":   ("a ", "1111 01aa aaaa a100"),
-    "brhc":   ("a ", "1111 01aa aaaa a101"),
-    "brtc":   ("a ", "1111 01aa aaaa a110"),
-    "brid":   ("a ", "1111 01aa aaaa a111"),
-    "bld":    ("rb", "1111 100a aaaa 0bbb"),
-    "bst":    ("rb", "1111 101a aaaa 0bbb"),
-    "sbrc":   ("rb", "1111 110a aaaa 0bbb"),
-    "sbrs":   ("rb", "1111 111a aaaa 0bbb"),
-}
+INSTRUCTION_SPECS = [
+    ("nop",    "  ", "0000 0000 0000 0000"),
+    ("movw",   "pp", "0000 0001 aaaa bbbb"),
+    ("muls",   "hh", "0000 0010 aaaa bbbb"),
+    ("mulsu",  "HH", "0000 0011 0aaa 0bbb"),
+    ("fmul",   "HH", "0000 0011 0aaa 1bbb"),
+    ("fmulsu", "HH", "0000 0011 1aaa 1bbb"),
+    ("fmuls",  "HH", "0000 0011 1aaa 0bbb"),
+    ("cpc",    "rr", "0000 01ba aaaa bbbb"),
+    ("sbc",    "rr", "0000 10ba aaaa bbbb"),
+    ("add",    "rr", "0000 11ba aaaa bbbb"),
+    ("lsl",    "r ", "0000 11Aa aaaa AAAA"),
+    ("cpse",   "rr", "0001 00ba aaaa bbbb"),
+    ("cp",     "rr", "0001 0110 0000 1111"),
+    ("sub",    "rr", "0001 10ba aaaa bbbb"),
+    ("adc",    "rr", "0001 11ba aaaa bbbb"),
+    ("rol",    "r ", "0001 11Aa aaaa AAAA"),
+    ("and",    "rr", "0010 00ba aaaa bbbb"),
+    ("tst",    "r ", "0010 00Aa aaaa AAAA"),
+    ("eor",    "rr", "0010 01ba aaaa bbbb"),
+    ("clr",    "r ", "0010 01Aa aaaa AAAA"),
+    ("or",     "rr", "0010 10ba aaaa bbbb"),
+    ("mov",    "rr", "0010 11ba aaaa bbbb"),
+    ("cpi",    "hk", "0011 bbbb aaaa bbbb"),
+    ("sbci",   "hk", "0100 bbbb aaaa bbbb"),
+    ("subi",   "hk", "0101 bbbb aaaa bbbb"),
+    ("ori",    "hk", "0110 bbbb aaaa bbbb"),
+    ("sbr",    "hk", "0110 bbbb aaaa bbbb"),
+    ("andi",   "hk", "0111 bbbb aaaa bbbb"),
+    ("cbr",    "hK", "0111 bbbb aaaa bbbb"),
+    ("lds",    "rA", "1001 000a aaaa 0000 bbbb bbbb bbbb bbbb"),
+    ("pop",    "r ", "1001 000a aaaa 1111"),
+    ("sts",    "Ar", "1001 001b bbbb 0000 aaaa aaaa aaaa aaaa"),
+    ("push",   "r ", "1001 001a aaaa 1111"),
+    ("ijmp",   "  ", "1001 0100 0000 1001"),
+    ("des",    "k ", "1001 0100 aaaa 1011"),
+    ("com",    "r ", "1001 010a aaaa 0000"),
+    ("neg",    "r ", "1001 010a aaaa 0001"),
+    ("swap",   "r ", "1001 010a aaaa 0010"),
+    ("inc",    "r ", "1001 010a aaaa 0011"),
+    ("asr",    "r ", "1001 010a aaaa 0101"),
+    ("lsr",    "r ", "1001 010a aaaa 0110"),
+    ("ror",    "r ", "1001 010a aaaa 0111"),
+    ("dec",    "r ", "1001 010a aaaa 1010"),
+    ("jmp",    "A ", "1001 010a aaaa 110a aaaa aaaa aaaa aaaa"),
+    ("call",   "A ", "1001 010a aaaa 111a aaaa aaaa aaaa aaaa"),
+    ("bset",   "b ", "1001 0100 0aaa 1000"),
+    ("sec",    "  ", "1001 0100 0000 1000"),
+    ("sez",    "  ", "1001 0100 0001 1000"),
+    ("eijmp",  "  ", "1001 0100 0001 1001"),
+    ("sen",    "  ", "1001 0100 0010 1000"),
+    ("sev",    "  ", "1001 0100 0011 1000"),
+    ("ses",    "  ", "1001 0100 0100 1000"),
+    ("seh",    "  ", "1001 0100 0101 1000"),
+    ("set",    "  ", "1001 0100 0110 1000"),
+    ("sei",    "  ", "1001 0100 0111 1000"),
+    ("bclr",   "b ", "1001 0100 1aaa 1000"),
+    ("clc",    "  ", "1001 0100 1000 1000"),
+    ("clz",    "  ", "1001 0100 1001 1000"),
+    ("cln",    "  ", "1001 0100 1010 1000"),
+    ("clv",    "  ", "1001 0100 1011 1000"),
+    ("cls",    "  ", "1001 0100 1100 1000"),
+    ("clh",    "  ", "1001 0100 1101 1000"),
+    ("clt",    "  ", "1001 0100 1110 1000"),
+    ("cli",    "  ", "1001 0100 1111 1000"),
+    ("ret",    "  ", "1001 0101 0000 1000"),
+    ("icall",  "  ", "1001 0101 0000 1001"),
+    ("reti",   "  ", "1001 0101 0001 1000"),
+    ("eicall", "  ", "1001 0101 0001 1001"),
+    ("sleep",  "  ", "1001 0101 1000 1000"),
+    ("break",  "  ", "1001 0101 1001 1000"),
+    ("wdr",    "  ", "1001 0101 1010 1000"),
+    ("adiw",   "Pk", "1001 0110 bbaa bbbb"),
+    ("sbiw",   "Pk", "1001 0111 bbaa bbbb"),
+    ("cbi",    "ib", "1001 1000 aaaa abbb"),
+    ("sbic",   "ib", "1001 1001 aaaa abbb"),
+    ("sbi",    "ib", "1001 1010 aaaa abbb"),
+    ("sbis",   "ib", "1001 1011 aaaa abbb"),
+    ("mul",    "rr", "1001 11ba aaaa bbbb"),
+    ("in",     "rI", "1011 0bba aaaa bbbb"),
+    ("out",    "Ir", "1011 1aab bbbb aaaa"),
+    ("rjmp",   "a ", "1100 aaaa aaaa aaaa"),
+    ("rcall",  "a ", "1101 aaaa aaaa aaaa"),
+    ("ldi",    "rk", "1110 bbbb aaaa bbbb"),
+    ("ser",    "r ", "1110 1111 aaaa 1111"),
+    ("brbs",   "ba", "1111 00bb bbbb baaa"),
+    ("brcs",   "a ", "1111 00aa aaaa a000"),
+    ("brlo",   "a ", "1111 00aa aaaa a000"),
+    ("breq",   "a ", "1111 00aa aaaa a001"),
+    ("brmi",   "a ", "1111 00aa aaaa a010"),
+    ("brvs",   "a ", "1111 00aa aaaa a011"),
+    ("brlt",   "a ", "1111 00aa aaaa a100"),
+    ("brhs",   "a ", "1111 00aa aaaa a101"),
+    ("brts",   "a ", "1111 00aa aaaa a110"),
+    ("brie",   "a ", "1111 00aa aaaa a111"),
+    ("brbc",   "ba", "1111 01bb bbbb baaa"),
+    ("brcc",   "a ", "1111 01aa aaaa a000"),
+    ("brsh",   "a ", "1111 01aa aaaa a000"),
+    ("brne",   "a ", "1111 01aa aaaa a001"),
+    ("brpl",   "a ", "1111 01aa aaaa a010"),
+    ("brvc",   "a ", "1111 01aa aaaa a011"),
+    ("brge",   "a ", "1111 01aa aaaa a100"),
+    ("brhc",   "a ", "1111 01aa aaaa a101"),
+    ("brtc",   "a ", "1111 01aa aaaa a110"),
+    ("brid",   "a ", "1111 01aa aaaa a111"),
+    ("bld",    "rb", "1111 100a aaaa 0bbb"),
+    ("bst",    "rb", "1111 101a aaaa 0bbb"),
+    ("sbrc",   "rb", "1111 110a aaaa 0bbb"),
+    ("sbrs",   "rb", "1111 111a aaaa 0bbb"),
+]
+
+
+INSTRUCTIONS = {}
+for op, operand_types, bit_pattern in INSTRUCTION_SPECS:
+    size = len(bit_pattern) // 16
+    operand1_type = OPERAND_TYPES[operand_types[0]]
+    operand2_type = OPERAND_TYPES[operand_types[1]]
+    variant = bit_pattern, operand1_type, operand2_type
+    if op in INSTRUCTIONS:
+        insn_size, variants = INSTRUCTIONS[op]
+        assert size == insn_size
+        variants.append(variant)
+    else:
+        INSTRUCTIONS[op] = size, [variant]
 
 
 class Label(collections.namedtuple("Label", ["symbol", "weak"])):
@@ -174,15 +188,12 @@ class Label(collections.namedtuple("Label", ["symbol", "weak"])):
         return super(Label, cls).__new__(cls, symbol, weak)
 
 
-class Insn(collections.namedtuple("Insn", ["op", "arg1", "arg2", "bit_pattern", "size", "operand1_type", "operand2_type"])):
+class Insn(collections.namedtuple("Insn", ["op", "arg1", "arg2", "size", "variants"])):
 
     def __new__(cls, op, arg1=Value(None, ValueType.NONE), arg2=Value(None, ValueType.NONE)):
         if op in INSTRUCTIONS:
-            operand_types, bit_pattern = INSTRUCTIONS[op]
-            operand1_type = OPERAND_TYPES[operand_types[0]]
-            operand2_type = OPERAND_TYPES[operand_types[1]]
-            size = len(bit_pattern) // 16
-            return super(Insn, cls).__new__(cls, op, arg1, arg2, bit_pattern, size, operand1_type, operand2_type)
+            size, variants = INSTRUCTIONS[op]
+            return super(Insn, cls).__new__(cls, op, arg1, arg2, size, variants)
         else:
             raise Exception("Unknown instruction: " + op)
 
@@ -206,7 +217,8 @@ def main():
             program += assemble(infile)
     insns = link(program)
     with open(args.outfile, "wb") as outfile:
-        write_program(outfile, insns)
+        for word in insns:
+            outfile.write(struct.pack("<H", word))
 
 
 def assemble(infile):
@@ -316,7 +328,7 @@ def fix(program, labels):
             addr += stmt.size
             arg1 = eval_arg(stmt.arg1, labels)
             arg2 = eval_arg(stmt.arg2, labels)
-            yield fix_insn(stmt, arg1, arg2, addr)
+            yield from fix_insn(stmt, arg1, arg2, addr)
 
 
 def eval_arg(arg, env):
@@ -328,15 +340,23 @@ def eval_arg(arg, env):
 
 
 def fix_insn(insn, arg1, arg2, current_addr):
-    op1 = insn.operand1_type.fit_value(arg1.val, arg1.typ, current_addr)
-    op2 = insn.operand2_type.fit_value(arg2.val, arg2.typ, current_addr)
-    return insn._replace(arg1=op1, arg2=op2)
+    valid_variant_words = []
+    for variant in insn.variants:
+        words = try_variant(variant, arg1, arg2, current_addr)
+        if words is not None:
+            valid_variant_words.append(words)
+    assert len(valid_variant_words) == 1
+    return valid_variant_words[0]
 
 
-def write_program(outfile, insns):
-    for insn in insns:
-        for word in insn_words(insn.bit_pattern, insn.arg1, insn.arg2):
-            outfile.write(struct.pack("<H", word))
+def try_variant(variant, arg1, arg2, current_addr):
+    bit_pattern, operand1_type, operand2_type = variant
+    try:
+        op1 = operand1_type.fit_value(arg1.val, arg1.typ, current_addr)
+        op2 = operand2_type.fit_value(arg2.val, arg2.typ, current_addr)
+    except AssertionError:
+        return None
+    return insn_words(bit_pattern, op1, op2)
 
 
 def insn_words(bit_pattern, arg1, arg2):
